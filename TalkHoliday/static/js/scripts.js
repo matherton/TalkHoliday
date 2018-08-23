5c6c7d08bf3b6519bf7e1378eb01b0e3cafedca0
matherton <mark@insightinternet.co.uk>
2018-08-23 13:12:44 +0100

    got slick slider working and added some intial styling

---
+++ TalkHoliday/static/js/scripts.js
@@ -0,0 +1,9 @@
+$('.post-scroller').slick({
+  infinite: true,
+  slidesToShow: 4, // Shows a three slides at a time
+  slidesToScroll: 1, // When you click an arrow, it scrolls 1 slide at a time
+  arrows: true, // Adds arrows to sides of slider
+  dots: true, // Adds the dots on the bottom
+	prevArrow: '<div class="slick-prev"><i class="fa fa-chevron-left"></i></div>',
+	nextArrow: '<div class="slick-next"><i class="fa fa-chevron-right"></i></div>'
+});
