22750653dad68be0414897ee2ce8c809a096c621
matherton <mark@insightinternet.co.uk>
2018-08-24 11:59:53 +0100

    added/styled what is tripsology section finished the mobile content

--- TalkHoliday/static/js/scripts.js
+++ TalkHoliday/static/js/scripts.js
@@ -2,11 +2,11 @@ var viewportWidth = window.innerWidth || document.documentElement.clientWidth ||
 if(viewportWidth <= 600) {
 	slideNo = 1;
 } else if(viewportWidth <= 1280) {
-	slideNo = 3;
+	slideNo = 2;
 } else if(viewportWidth <= 1920) {
-	slideNo = 4;
+	slideNo = 3;
 } else {
-	slideNo = 5;
+	slideNo = 4;
 }
 
 $('.post-scroller').slick({
@@ -14,7 +14,7 @@ $('.post-scroller').slick({
   slidesToShow: slideNo, // Shows a three slides at a time
   slidesToScroll: 1, // When you click an arrow, it scrolls 1 slide at a time
   arrows: true, // Adds arrows to sides of slider
-  dots: true, // Adds the dots on the bottom
+  //dots: true,  Adds the dots on the bottom
 	prevArrow: '<div class="slick-prev"><i class="fa fa-chevron-left"></i></div>',
 	nextArrow: '<div class="slick-next"><i class="fa fa-chevron-right"></i></div>'
 });
